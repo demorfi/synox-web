7347db45e743d4b92748b4153fabaad2247683d5
demorfi <demorfi@gmail.com>
2025-12-14 11:28:54 +1000

    change: rename router to routes directory

--- resource/assets/bootstrap.js
+++ resource/assets/bootstrap.js
@@ -5,6 +5,6 @@ export {default as HttpCommon} from './api/http-common';
 export {default as BootstrapVueNext} from 'bootstrap-vue-next';
 export {default as VueProgressBar} from '@aacassandra/vue3-progressbar';
 export {default as VueProgressBarExt} from './plugins/progressBarExt';
-export {default as Router} from './router';
+export {default as Router} from './routes';
 export {default as Store} from './store';
 export {default as PrefersScheme} from './plugins/prefersScheme';
